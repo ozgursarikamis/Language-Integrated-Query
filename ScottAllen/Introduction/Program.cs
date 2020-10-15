7fcb0e4caf08c558685f1dcd81f9ff4c3a92bf25
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-15 09:21:58 +0300

    querying with LINQ

--- ScottAllen/Introduction/Program.cs
+++ ScottAllen/Introduction/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Introduction
 {
@@ -10,6 +11,23 @@ namespace Introduction
         {
             const string path = @"C:\Windows";
             ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("***");
+            ShowLargeFilesWithLinq(path);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = 
+                from file in new DirectoryInfo(path).GetFiles()
+                orderby file.Length descending
+                select file;
+
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name, -20} - {file.Length, 10:N0}");
+            }
         }
 
         private static void ShowLargeFilesWithoutLinq(string path)
