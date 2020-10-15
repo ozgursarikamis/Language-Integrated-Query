8180b6484e587219f1f9979316a0a95c46870885
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-15 16:57:54 +0300

    Queries project added

---
+++ ScottAllen/Queries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Title = "The Dark Night", Rating = 8.9f, Year = 2008},
+                new Movie {Title = "The King's Speech", Rating = 8.0f, Year = 2010},
+                new Movie {Title = "Casablanca", Rating = 8.5f, Year = 1942},
+                new Movie {Title = "Star wars V", Rating = 8.7f, Year = 1980},
+            };
+
+            var query = movies.Where(x => x.Year > 2000);
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
