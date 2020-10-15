72f7379f53f858aeddbee8bb6870dc030f6d3677
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-15 19:31:28 +0300

    Process csv file with Extension Methods and Query-syntax method

--- ScottAllen/Cars/Program.cs
+++ ScottAllen/Cars/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Cars
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Cars
 {
     internal static class Program
     {
         private static void Main()
         {
+            var cars = ProcessFile("fuel.csv");
 
+            var query = from car in cars
+                orderby car.Combined ascending, car.Name ascending
+                select car;
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
         }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            // Extension Method Syntax:
+            //return File.ReadAllLines(path)
+            //    .Skip(1)
+            //    .Where(line => line.Length > 1)
+            //    .Select(Car.ParseFromCsv)
+            //    .ToList();
+
+            // Linq Query Syntax:
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+
+            return query.ToList();
+        } 
     }
 }
