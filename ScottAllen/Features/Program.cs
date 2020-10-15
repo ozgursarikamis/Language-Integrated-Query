5f4bced79ee11a1074cd4c39c31d753b7672f002
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-15 09:34:46 +0300

    foreach : the hard way

---
+++ ScottAllen/Features/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee{ Id = 1, Name = "Scott" }, 
+                new Employee{ Id = 2, Name = "Chris" }
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>
+            {
+                new Employee {Id = 3, Name = "Alex"}
+            };
+
+            foreach (var person in developers)
+            {
+                Console.WriteLine($"{person.Name} - {person.Id}");
+            }
+
+            Console.WriteLine("***");
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine($"{enumerator.Current.Name} - {enumerator.Current.Id}");
+            }
+        }
+    }
+}
